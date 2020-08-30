666e022d45fa9324ec32883520eaa0266c08c8ed
Kyeil <kyeil.k.lee@gmail.com>
2020-08-30 13:11:21 +0900

    Enumerize vibration keys

--- UnityConverter/Assets/BravesVibUnityConverter/Scripts/VibManager.cs
+++ UnityConverter/Assets/BravesVibUnityConverter/Scripts/VibManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class VibManager : MonoBehaviour
 {
@@ -36,8 +37,7 @@ public class VibManager : MonoBehaviour
     private void ReadVibDataFromJson()
     {
         TextAsset[] vibDataFiles = Resources.LoadAll<TextAsset>("VibData");
-
-
+        EnumerizeKeys(vibDataFiles);
 
         vibDictionary = new Dictionary<string, OVRHapticsClip>();
 
@@ -48,7 +48,35 @@ public class VibManager : MonoBehaviour
             OVRHapticsClip _clip = new OVRHapticsClip(_bytes, _bytes.Length);
             vibDictionary.Add(vibDataFiles[_i].name, _clip);               //add these data sets to dictionary
         }
+
+        
     }
+
+
+    private void EnumerizeKeys(TextAsset[] _vibDataFiles)
+    {
+        string enumTitle = "VibKeys";                   
+        List<string> enumEntries = new List<string>();                  //add vibdata names to string list to enumerize them
+        for(int _i = 0 ; _i < _vibDataFiles.Length ; _i ++)
+        {        
+            enumEntries.Add(_vibDataFiles[_i].name);
+        }
+
+        using (StreamWriter streamWriter = new StreamWriter("Assets/BravesVibUnityConverter/Scripts/" + enumTitle + ".cs"))
+        {
+            streamWriter.WriteLine("public enum " + enumTitle);
+            streamWriter.WriteLine("{");
+            for(int _i = 0 ; _i < enumEntries.Count ; _i++)
+            {
+                streamWriter.WriteLine("\t" + enumEntries[_i] + ",");
+            }
+            streamWriter.WriteLine("}");
+        }
+
+        UnityEditor.AssetDatabase.Refresh();
+    }
+
+
 
     // String to byte array
     private byte[] StringToByte(string str)
