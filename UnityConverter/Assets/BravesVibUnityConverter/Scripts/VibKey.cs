817051dd11722dccfc20e7af5540e9f55ee2967d
chang <amiesuie@naver.com>
2020-09-01 16:35:56 +0900

    Parsing change

--- UnityConverter/Assets/BravesVibUnityConverter/Scripts/VibKey.cs
+++ UnityConverter/Assets/BravesVibUnityConverter/Scripts/VibKey.cs
@@ -1,6 +1,8 @@
 public enum VibKey
 {
+	Sample4,
 	VibSample1,
 	VibSample2,
 	VibSample3,
+	VibSample5,
 }
--- UnityConverter/Assets/BravesVibUnityConverter/Scripts/VibrationTester.cs
+++ UnityConverter/Assets/BravesVibUnityConverter/Scripts/VibrationTester.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class VibrationTester : MonoBehaviour
 {
+    public string _str;
 
     private void Update()
     {
+
         //index trigger down
 		if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
 		{
-            OVRHaptics.LeftChannel.Mix(VibManager.vibDictionary[VibManager.testKey]);
+            
+            string[] strigns = _str.Split(',');
+            byte[] bytes = new byte[strigns.Length];
+            for(int i = 0; i < strigns.Length; i++)
+            {
+                Debug.Log(int.Parse(strigns[i]) + "  " + System.Convert.ToByte(int.Parse(strigns[i])));
+                bytes[i] = System.Convert.ToByte(int.Parse(strigns[i]));
+            }
+
+
+            OVRHapticsClip _clip = new OVRHapticsClip(bytes, bytes.Length);
+            OVRHaptics.LeftChannel.Mix(_clip);
 		}
 		if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
 		{
