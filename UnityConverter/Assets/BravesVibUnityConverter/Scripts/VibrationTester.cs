aab37f58761c6ddb30dc96c8fa9f30f0195c633d
Kyeil <kyeil.k.lee@gmail.com>
2020-09-02 10:44:30 +0900

    //  VibManager data parse

--- UnityConverter/Assets/BravesVibUnityConverter/Scripts/VibrationTester.cs
+++ UnityConverter/Assets/BravesVibUnityConverter/Scripts/VibrationTester.cs
@@ -12,10 +12,6 @@ public class VibrationTester : MonoBehaviour
         //index trigger down
 		if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
 		{
-            
-
-
-
             string[] strigns = _str.Split(',');
             byte[] bytes = new byte[strigns.Length];
             for(int i = 0; i < strigns.Length; i++)
@@ -27,6 +23,8 @@ public class VibrationTester : MonoBehaviour
 
             OVRHapticsClip _clip = new OVRHapticsClip(bytes, bytes.Length);
             OVRHaptics.LeftChannel.Mix(_clip);
+
+            //OVRHaptics.LeftChannel.Mix(VibManager.vibDictionary[VibManager.testKey]);
 		}
 		if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
 		{
